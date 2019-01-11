3f8dd4e64a210b3a457eed4d94bd624c4e6abfba
vikash agrawal <vikash.agrawal@gep.com>
2019-01-11 20:12:51 +0530

    timely commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+// import * as wjcGridDetail from 'wijmo/wijmo.grid.detail';
+
+// import * as wjcCore from 'wijmo/wijmo';
+// import * as wjcGrid from 'wijmo/wijmo.grid';
+import { getData } from '../model/data.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', '../../node_modules/wijmo/styles/wijmo.scss'
+]
 })
 export class AppComponent {
-  title = 'ng5';
+  data = getData();
 }
