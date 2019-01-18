e59834663c8d7f5ca41b1e0cd60f00596cbb2fa8
vikash agrawal <vikash.agrawal@gep.com>
2019-01-18 20:39:37 +0530

    change for demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,4 +13,20 @@ import { getData } from '../model/data.js';
 })
 export class AppComponent {
   data = getData();
+  onload = function() {
+
+    // create some random data
+    var countries = 'US,Germany,UK,Japan,Italy,Greece'.split(',');
+    var products = 'Piano,Violin,Flute,Guitar,Cello'.split(',');
+    var data = [];
+    for (var i = 0; i < 10000; i++) {
+      data.push({
+        id: i,
+        country: countries[i % countries.length],
+        product: products[i % products.length],
+        sales: Math.random() * 10000,
+        expenses: Math.random() * 5000
+      });
+    }
+  }
 }
